8c21600658afe9c6ab4a690e67f23b9671be0419
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2025-12-02 08:56:18 +0100

    adding courses and crud for courses

--- schoolCrud.py
+++ schoolCrud.py
@@ -4,7 +4,7 @@ import sqlite3
 connection = sqlite3.connect("school.db")
 cursor = connection.cursor()
 
-# 2. Create a table for students
+# 2. Create tables
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,25 +22,70 @@ CREATE TABLE IF NOT EXISTS teachers (
     phone INTEGER
 )
 """)
+cursor.execute("""CREATE TABLE IF NOT EXISTS courses(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name TEXT NOT NULL,
+               description TEXT,
+               year INTEGER
+               )""" )
 
 # 3. Insert some data
-cursor.execute("INSERT INTO students (name, age, grade) VALUES (?, ?, ?)", ("Anna", 16, "10A"))
-cursor.execute('UPDATE students SET name = "ID10" WHERE id = 10')
+#Student Data
+#cursor.execute("INSERT INTO students (name, age, grade) VALUES (?, ?, ?)", ("Anna", 16, "10A"))
 
-cursor.execute("INSERT INTO teachers (name, email, phone) VALUES (?, ?, ?)", ("Anna", "anna@gmail.com", "123456789"))
-cursor.execute('UPDATE teachers SET phone = "1001" WHERE id = 10')
-cursor.execute('DELETE FROM teachers WHERE id > 10')
+#Teacher Data
+#cursor.execute("INSERT INTO teachers (name, email, phone) VALUES (?, ?, ?)", ("Anna", "anna@gmail.com", "123456789"))
+
+#Course Data
+#cursor.execute("INSERT INTO courses (name, description, year) VALUES (?, ?, ?)", ("Mathematics", "Basic Math Course", 10))
+
+# 4. Update and Delete data
+
+# Update Student
+#cursor.execute('UPDATE students SET name = "ID10" WHERE id = 10')
+
+# Delete Student
+#cursor.execute('DELETE FROM students WHERE id > 10')
+
+# Update Teacher
+#cursor.execute('UPDATE teachers SET name = "ID10" WHERE id = 10')
+
+# Delete Teacher
+# cursor.execute('DELETE FROM teachers WHERE id > 10')
+
+
+# Update Course
+#cursor.execute('UPDATE courses SET name = "ID10" WHERE id = 1')
+
+# Delete Course
+#cursor.execute('DELETE FROM courses WHERE id = 1')
 
 # Commit the changes
 connection.commit()
 
-# 4. Query the data
-cursor.execute("SELECT * FROM teachers")
-teachers = cursor.fetchall()
+# 5. Read data
+# Read Students
+#cursor.execute("SELECT * FROM students")
+#students = cursor.fetchall()
+#print("List of students:")
+#for teacher in students:
+ #   print(student)
 
-print("List of teachers:")
-for teacher in teachers:
-    print(teacher)
+# Read Teachers
+#cursor.execute("SELECT * FROM teachers")
+#teachers = cursor.fetchall()
+#print("List of teachers:")
+#for teacher in teachers:
+ #   print(teacher)
 
-# 5. Close the connection
+# Read Courses 
+cursor.execute("SELECT * FROM courses")
+courses = cursor.fetchall()
+print("List of courses:")
+for course in courses:
+    print(course)
+
+
+
+# 6. Close the connection
 connection.close()
